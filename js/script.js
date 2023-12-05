bdd781a86c80b7e355563906c5d20f79a266e609
Manish Mali <manishmali448278@gmail.com>
2023-12-05 23:40:38 +0530

    white space problem solved

--- js/script.js
+++ js/script.js
@@ -1,36 +1,5 @@
-// window.addEventListener('load', () => {
-//   let portfolioContainer = select('.portfolio-container');
-//   if (portfolioContainer) {
-//     let portfolioIsotope = new Isotope(portfolioContainer, {
-//       itemSelector: '.portfolio_item'
-//     });
-
-//     let portfolioFilters = select('#portfolio-flters li', true);
-
-//     on('click', '#portfolio-flters li', function(e) {
-//       e.preventDefault();
-//       portfolioFilters.forEach(function(el) {
-//         el.classList.remove('filter-active');
-//       });
-//       this.classList.add('filter-active');
-
-//       portfolioIsotope.arrange({
-//         filter: this.getAttribute('data-filter')
-//       });
-//       portfolioIsotope.on('arrangeComplete', function() {
-//         AOS.refresh()
-//       });
-//     }, true);
-//   }
-
-// });
-
-// Navbar Script For Toggle Bar -- Harshil
-
-(function () {
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#nav_links').classList.toggle('navbar-mobile')
-    this.classList.toggle('fa-bars')
-    this.classList.toggle('fa-xmark')
+$(document).ready(function(){
+  $('.mobile-nav-toggle').click(function(){
+    $('ul').toggleClass('nav-active');
   })
-});
+})
